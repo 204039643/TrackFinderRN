a1f73bd567477a88c7d03d693711df595d5331f8
Justin Sykes <atlsykes1@att.net>
2021-05-13 09:04:32 -0400

    added LogInScreen.js component and updated RN navigation in App.js, updated some props on text inputs for SignUpScreen.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {Button, View, Text} from 'react-native'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import SignUpScreen from './src/screens/SignUpScreen'
+import LogInScreen from './src/screens/LogInScreen'
 
 function HomeScreen ({navigation}) {
   return (
@@ -11,6 +12,7 @@ function HomeScreen ({navigation}) {
       <Text>
         Welcome to Trackfinder 2.0! This is the HomeScreen placeholder.
       </Text>
+      <Button title='Log in!' onPress={() => navigation.navigate('LogIn')} />
       <Button title='Sign Up!' onPress={() => navigation.navigate('SignUp')} />
     </View>
   )
@@ -23,6 +25,7 @@ function App () {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='LogIn' component={LogInScreen} />
         <Stack.Screen name='SignUp' component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
